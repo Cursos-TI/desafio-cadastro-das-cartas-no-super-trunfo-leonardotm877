2fbd3eee128ecfa8c40bcd773e296c36891cedde
leonardotm877 <ltm.leonardomiguel@gmail.com>
2025-01-29 23:05:37 +0000

    Inserindo Dados Primitivos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,7 @@ printf("codigo: \n");
 scanf("%d", &codigo);
 
 printf("nome: \n");
-printf("%s", &nome);
+scanf("%s", &nome);
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
