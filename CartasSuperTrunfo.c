d4fd62fd5c4c7202c0c16852af6a971f7db46a52
leonardotm877 <ltm.leonardomiguel@gmail.com>
2025-02-07 23:25:07 +0000

    1° Estado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,31 +8,133 @@
 
 int main() {
     printf("Desafio Super Trunfo\n");
-         int codigo = 01;
-         char cidade[30];
-         float populacao;
-         double area;
-         float PIB;
-         int turistico;
+      // Declaração de variáveis para armazenar nomes de países
+    char pais[20] = "Brasil";
+    char pais_1[20] = "EUA";
+    char pais_2[20] = "Rússia";
+    char pais_3[20] = "China";
+    char codigo[90]; // Variável para armazenar código do estado/cidade
+    float populacao; // Variável para armazenar população
+    float area; // Variável para armazenar a área
+    double pib; // Variável para armazenar o PIB
+    float turismo; // Variável para armazenar os pontos turísticos
 
-printf("codigo: ");
-scanf("%d", &codigo);
+// 1° País - Estado A - 1° Cidade
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Brasil \n");
+printf ("Código da carta: A01 \n");
 
-printf("cidade: ");
-scanf("%s", &cidade);
+printf ("População: ");
+scanf ("%f", &populacao);
 
-printf("populacao: ");
-scanf("%f", &populacao);
+printf ("Área: ");
+scanf ("%f", &area);
 
-printf("area:");
-scanf("%2f", area);
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais);
+printf ("Código da carta: A01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 1° País - Estado A (Brasil) - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Brasil \n");
+printf ("Código da carta: A02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf("Pontos Turísticos: ");
+scanf("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais);
+printf ("Código da carta: A02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 1° País - Estado A (Brasil) - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Brasil \n");
+printf ("Código da carta: A03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais);
+printf ("Código da carta: A03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3f \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 1° País - Estado A (Brasil) - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Brasil \n");
+printf ("Código da carta: A04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais);
+printf ("Código da Carta: A04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO PRIMEIRO ESTADO
+
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, poulação, área, etc.
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
