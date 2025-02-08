02af4ffc11bb439b7dc8f38b73e61d05c67dbbed
leonardotm877 <ltm.leonardomiguel@gmail.com>
2025-02-08 00:14:07 +0000

    8 estados,

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -237,6 +237,667 @@ printf ("Pontos Turísticos: %f \n", turismo);
 printf ("\n");
 // FINAL DO SEGUNDO ESTADO
 
+// 2° País - Estado C - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: C01 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: C01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 2° País - Estado C - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: C02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: C02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("\n");
+
+// 2° País - Estado C - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: C03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: C03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("\n");
+
+// 2° País - Estado C - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: C04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: C04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO TERCEIRO ESTADO
+
+
+// 2° País - Estado D - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: D01 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: D01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3f \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 2° País - Estado D - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: D02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("Países: %s \n", pais_1);
+printf ("Código da carta: D02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 2° País - Estado D - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: D03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("Países: %s \n", pais_1);
+printf ("Código da carta: D03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 2° País - Estado D - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: EUA \n");
+printf ("Código da carta: D04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_1);
+printf ("Código da carta: D04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3f \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO QUARTO ESTADO
+
+
+// 3° País - Estado E - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: E01 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: E01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado E - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: E02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: E02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3f \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado E - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: E03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: E03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado E - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: E04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: E04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO QUINTO ESTADO
+
+
+// 3° País - Estado F - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: F01 \n");
+
+printf("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: F01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado F - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: F02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: F02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado F - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: F03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: F03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 3° País - Estado F - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: Rússia \n");
+printf ("Código da carta: F04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_2);
+printf ("Código da carta: F04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f", turismo);
+printf ("\n");
+// FINAL DO SEXTO ESTADO
+
+
+// 4° País - Estado G - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: G01 \n");
+
+printf ("População ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: G01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+
+// 4° País - Estado G - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: G02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: G02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 4° País - Estado G - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: G03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: G03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3f \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 4° País - Estado G - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: G04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: G04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO SÉTIMO ESTADO
+
+
+// 4° País - Estado H - 1° Cidade (01)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: H01 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: H01 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 4° País - Estado H - 2° Cidade (02)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: H02 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("Pais: %s \n", pais_3);
+printf ("Código da carta: H02 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+
+// 4° País - Estado H - 3° Cidade (03)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: H03 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: H03 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Área: %.0f km² \n ", area);
+printf ("PIB: R$ %.3lf \n ", pib);
+printf ("Pontos Turísticos: %.0f \n ", turismo);
+printf ("\n");
+
+// 4° País - Estado H - 4° Cidade (04)
+// Coleta as informações de diferentes cidades deste país
+printf ("País: China \n");
+printf ("Código da carta: H04 \n");
+
+printf ("População: ");
+scanf ("%f", &populacao);
+
+printf ("Área: ");
+scanf ("%f", &area);
+
+printf ("PIB: ");
+scanf ("%lf", &pib);
+
+printf ("Pontos Turísticos: ");
+scanf ("%f", &turismo);
+
+// Exibe as informações digitadas pelo usuário
+printf ("\n");
+printf ("País: %s \n", pais_3);
+printf ("Código da carta: H04 %s \n", codigo);
+printf ("População: %.3f \n", populacao);
+printf ("Area: %.0f km² \n", area);
+printf ("PIB: R$ %.3lf \n ", pib);
+printf ("Pontos Turísticos: %.0f \n", turismo);
+printf ("\n");
+// FINAL DO OITAVO ESTADO
+
+return 0;
+
+}
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
